7b274632fd1f756c616508a5d5d88c693e5a0fda
n0tx <rcandra91@msn.com>
2023-11-16 08:15:04 +0700

    Open block CORS frontend 'Access-Control-Allow-Origin'

--- contact-app-backend/src/main/java/com/riki/contactappbackend/web/PersonResource.java
+++ contact-app-backend/src/main/java/com/riki/contactappbackend/web/PersonResource.java
@@ -4,12 +4,14 @@ import com.riki.contactappbackend.dto.response.PersonListResponseDTO;
 import com.riki.contactappbackend.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class PersonResource {
 
